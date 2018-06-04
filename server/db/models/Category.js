0ed176f1610c124acd6192e7220c0727c22b5644
Sean Lee <seanslee92@gmail.com>
2018-06-04 15:48:37 -0400

    added Category model

---
+++ server/db/models/Category.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Category = db.define('category', {
+    name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+})
+
+module.exports = Category
