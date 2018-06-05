fc86e3d2570b541143c3b22917ba7ff49b130848
Blake Spencer <blake.spencer1@gmail.com>
2018-06-05 15:26:50 -0400

    drinks Api ready for testing

--- server/api/drinks.js
+++ server/api/drinks.js
@@ -1,9 +1,51 @@
 const router = require('express').Router()
-const {Drink, Brand, Category} = require('../db/models')
+const { Drink, Brand, Category } = require('../db/models')
 module.exports = router
 
-router.get('/', (req, res, next) => {
-  Drink.findAll()
-    .then(users => res.json(users))
-    .catch(next)
+router.get('/', async (req, res, next) => {
+  try {
+    const data = Drink.findAll()
+    res.json(data)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// router.get('/:drinkId', async (req, res, next) )
+
+// CRUD
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newDrink = await Drink.create(req.body)
+    res.json(newDrink)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:drinkId', async (req, res, next) => {
+  try {
+    const data = Drink.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.send(data)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:drinkId', async (req, res, next) => {
+  try {
+    await Drink.destory({
+      where: {
+        id: req.params.drinkId
+      }
+    })
+    res.send(`successfully destroyed ${req.params.drinkId}`)
+  } catch (error) {
+    next(error)
+  }
 })
