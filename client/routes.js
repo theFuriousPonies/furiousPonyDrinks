96f3044f73828dee168a3e49e89bd627321d5813
Chloe Chong <chloechong.us@gmail.com>
2018-06-06 15:26:11 -0400

    fixing routes

--- client/routes.js
+++ client/routes.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Route, Switch } from 'react-router-dom'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import { Login, Signup, UserHome } from './components'
 import { me } from './store'
 
+import { getBrands } from './store/brand'
 /**
  * COMPONENT
  */
@@ -37,13 +38,12 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
-  }
-}
+const mapState = ({ user, brands, categories, drinks }) => ({
+  user,
+  brands,
+  categories,
+  drinks
+})
 
 const mapDispatch = dispatch => {
   return {
@@ -61,11 +61,3 @@ export default withRouter(
     mapDispatch
   )(Routes)
 )
-
-/**
- * PROP TYPES
- */
-Routes.propTypes = {
-  loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
