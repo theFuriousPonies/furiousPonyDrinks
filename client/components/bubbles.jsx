fe99c25d288a9e408aa3e9d48f765d2f087fc100
Blake Spencer <blake.spencer1@gmail.com>
2018-06-08 00:52:23 -0400

    positioning of bubbles

--- client/components/bubbles.jsx
+++ client/components/bubbles.jsx
@@ -3,11 +3,19 @@ import React from 'react'
 const Bubbles = () => {
   return (
     <svg className="bubble-containter">
-      <g className="bubble">
-        <circle className="bubble1" cx="500" cy="-120" r="25" />
-        <circle className="bubble2" cx="300" cy="-200" r="15" />
-        <circle className="bubble2" cx="200" cy="-400" r="10" />
-        <circle className="bubble2" cx="700" cy="-250" r="7" />
+      <g className="bubble-thing">
+        <g className="bubble">
+          <circle className="bubble1" cx="500" cy="-120" r="25" />
+        </g>
+        <g className="bubble">
+          <circle className="bubble2" cx="300" cy="-200" r="15" />
+        </g>
+        <g className="bubble">
+          <circle className="bubble3" cx="200" cy="-400" r="10" />
+        </g>
+        <g className="bubble">
+          <circle className="bubble4" cx="700" cy="-250" r="7" />
+        </g>
       </g>
     </svg>
   )
