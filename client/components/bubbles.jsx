4ae58541d031a0a62568bcc163a4afe3c4903a73
Blake Spencer <blake.spencer1@gmail.com>
2018-06-08 01:09:24 -0400

    changed speed of bubbles

--- client/components/bubbles.jsx
+++ client/components/bubbles.jsx
@@ -4,17 +4,25 @@ const Bubbles = () => {
   return (
     <svg className="bubble-containter">
       <g className="bubble-thing">
-        <g className="bubble">
-          <circle className="bubble1" cx="500" cy="-120" r="25" />
+        <g className="delayed">
+          <g className="bubble">
+            <circle className="bubble1" cx="500" cy="-120" r="25" />
+          </g>
         </g>
-        <g className="bubble">
-          <circle className="bubble2" cx="300" cy="-200" r="15" />
+        <g>
+          <g className="bubble">
+            <circle className="bubble2" cx="300" cy="-200" r="15" />
+          </g>
         </g>
-        <g className="bubble">
-          <circle className="bubble3" cx="200" cy="-400" r="10" />
+        <g>
+          <g className="bubble">
+            <circle className="bubble3" cx="200" cy="-400" r="10" />
+          </g>
         </g>
-        <g className="bubble">
-          <circle className="bubble4" cx="700" cy="-250" r="7" />
+        <g>
+          <g className="bubble">
+            <circle className="bubble4" cx="700" cy="-250" r="7" />
+          </g>
         </g>
       </g>
     </svg>
