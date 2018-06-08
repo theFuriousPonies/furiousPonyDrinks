6d5ae535a8451a85bf499b3097353bae6a96e92a
Blake Spencer <blake.spencer1@gmail.com>
2018-06-08 01:51:14 -0400

    added wobble to individual bubbles

--- client/components/bubbles.jsx
+++ client/components/bubbles.jsx
@@ -4,24 +4,24 @@ const Bubbles = () => {
   return (
     <svg className="bubble-containter">
       <g className="bubble-thing">
-        <g className="delayed">
-          <g className="bubble">
-            <circle className="bubble1" cx="500" cy="-120" r="25" />
+        <g className="bubble-box" id="bubble-delayed-big">
+          <g className="bubble" id="wobble-delayed">
+            <circle className="bubble1" cx="500" cy="-100" r="25" />
           </g>
         </g>
-        <g>
-          <g className="bubble">
-            <circle className="bubble2" cx="300" cy="-200" r="15" />
+        <g className="bubble-box">
+          <g className="bubble" id="wobble-delayed-fast">
+            <circle className="bubble2" cx="300" cy="-100" r="15" />
           </g>
         </g>
-        <g>
-          <g className="bubble">
-            <circle className="bubble3" cx="200" cy="-400" r="10" />
+        <g className="bubble-box" id="bubble-small-delayed">
+          <g className="bubble" id="wobble-fast">
+            <circle className="bubble3" cx="200" cy="-100" r="10" />
           </g>
         </g>
-        <g>
+        <g className="bubble-box" id="bubble-small-delay">
           <g className="bubble">
-            <circle className="bubble4" cx="700" cy="-250" r="7" />
+            <circle className="bubble4" cx="700" cy="-100" r="7" />
           </g>
         </g>
       </g>
