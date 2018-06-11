8358d99fa894310d2b9dc4624ba771aa438dc270
Kirk Sullivan <kirk_135@hotmail.com>
2018-06-11 17:22:11 -0400

    Update: Cleaned up the Checkout and SplitForm components

--- server/api/stripe.js
+++ server/api/stripe.js
@@ -3,14 +3,9 @@ const router = require('express').Router()
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 var stripe = require('stripe')('sk_test_jHXyTXBJqigeT3bsa7QekdXT')
 
-// Token is created using Checkout or Elements!
-// Get the payment token ID submitted by the form:
-
 router.post('/', async (req, res, next) => {
   try {
-    //the token will be used when we go live
     const { token } = req.body
-    console.log(token)
     const charge = await stripe.charges.create({
       amount: 999,
       currency: 'usd',
